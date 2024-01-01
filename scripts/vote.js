90b57ff2f2691083465743dd3d1b02a9c1f99b8c
siid sharma <siid123ck@gmail.com>
2024-01-01 22:33:22 +1100

    contract ad scripts complete

---
+++ scripts/vote.js
@@ -0,0 +1,37 @@
+import * as fs from "fs"
+import { network, ethers } from "hardhat"
+import { proposalsFile, developmentChains, VOTING_PERIOD } from "../helper-hardhat-config"
+import { moveBlocks } from "../utils/move-blocks"
+
+
+async function main() {
+  const proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf8"))
+  const chainId = network.config && network.config.chainId ? network.config.chainId.toString() : '';
+
+  // Use optional chaining to handle the case where proposals[chainId] is undefined
+  const proposalId = proposals[chainId]?.at(-1);
+  const voteWay = 1
+  const reason = "I lika do da cha cha"
+  await vote(proposalId, voteWay, reason)
+}
+
+// 0 = Against, 1 = For, 2 = Abstain for this example
+export async function vote(proposalId, voteWay, reason) {
+  console.log("Voting...")
+  const governor = await ethers.getContract("GovernorContract")
+  const voteTx = await governor.castVoteWithReason(proposalId, voteWay, reason)
+  const voteTxReceipt = await voteTx.wait(1)
+  console.log(voteTxReceipt.events[0].args.reason)
+  const proposalState = await governor.state(proposalId)
+  console.log(`Current Proposal State: ${proposalState}`)
+  if (developmentChains.includes(network.name)) {
+    await moveBlocks(VOTING_PERIOD + 1)
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
